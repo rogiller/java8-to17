ff4834f8b006079130d405d0fe77a4b1dddf0b43
Roger Diller <roger@flexrentalsolutions.com>
2023-04-05 15:28:43 -0400

    initial commit

---
+++ src/main/java/com/example/java8to17/Java8To17Application.java
@@ -0,0 +1,13 @@
+package com.example.java8to17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8To17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8To17Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java8to17/Java8To17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java8to17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8To17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
